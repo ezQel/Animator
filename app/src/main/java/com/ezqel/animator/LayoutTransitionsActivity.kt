604bf9a5dcf73073f64d5ed28f538b5c8e439735
ezQel <ezekiel3499@gmail.com>
2019-11-19 14:18:26 +0300

    Performed Scene Transitions from code

--- app/src/main/java/com/ezqel/animator/LayoutTransitionsActivity.kt
+++ app/src/main/java/com/ezqel/animator/LayoutTransitionsActivity.kt
@@ -2,13 +2,14 @@ package com.ezqel.animator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.transition.Scene
-import android.transition.Transition
-import android.transition.TransitionInflater
-import android.transition.TransitionManager
+import android.transition.*
+import android.view.animation.BounceInterpolator
 import kotlinx.android.synthetic.main.activity_layout_transitions.*
 
 class LayoutTransitionsActivity : AppCompatActivity() {
+    lateinit var transitionSet: TransitionSet
+    lateinit var fadeInTransition: Fade
+    lateinit var changeBoundsTransition: ChangeBounds
     lateinit var sceneOne:Scene
     lateinit var sceneTwo:Scene
     lateinit var currentScene:Scene
@@ -26,7 +27,20 @@ class LayoutTransitionsActivity : AppCompatActivity() {
         currentScene = sceneOne
 
         // create a transition object to define transition of choice
-        transition = TransitionInflater.from(this).inflateTransition(R.transition.auto_transition)
+        changeBoundsTransition = ChangeBounds()
+        changeBoundsTransition.duration = 500
+        changeBoundsTransition.interpolator = BounceInterpolator()
+        fadeInTransition = Fade(Fade.MODE_IN)
+        fadeInTransition.startDelay = 500
+        fadeInTransition.duration = 2000
+        fadeInTransition.addTarget(R.id.disclaimer)
+        fadeInTransition.addTarget(R.id.disclaimer_text)
+
+        transitionSet = TransitionSet()
+        transitionSet.ordering = TransitionSet.ORDERING_SEQUENTIAL
+        transitionSet.addTransition(changeBoundsTransition)
+        transitionSet.addTransition(fadeInTransition)
+//        transition = TransitionInflater.from(this).inflateTransition(R.transition.example_0)
 
         sceneLayout.setOnClickListener{
             sceneTransition()
@@ -35,10 +49,10 @@ class LayoutTransitionsActivity : AppCompatActivity() {
 
     private fun sceneTransition() {
         currentScene = if (currentScene === sceneOne){
-            TransitionManager.go(sceneTwo, transition)
+            TransitionManager.go(sceneTwo, transitionSet)
             sceneTwo
         } else {
-            TransitionManager.go(sceneOne, transition)
+            TransitionManager.go(sceneOne, transitionSet)
             sceneOne
         }
 
